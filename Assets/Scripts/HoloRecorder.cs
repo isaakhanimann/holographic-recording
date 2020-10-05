73423b60b54ca96a5232dbd787a10e2374092884
isaak <isaak.hanimann@gmail.com>
2020-10-05 15:12:24 +0200

    Create HoloRecorder and HoloRecording Class
    - Change VS Code version to 1.1.3
    - Animation recording and voice recording
    - Untested

---
+++ Assets/Scripts/HoloRecorder.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+
+public class HoloRecorder : MonoBehaviour
+{
+
+    public static HoloRecorder instance;
+
+    private IMixedRealityInputSystem inputSystem = null;
+    private InputRecordingService animationRecorder;
+    private AudioClip currentAudioClip;
+    private const string MICROPHONE_NAME = "Built-in Microphone";
+
+    public void Initialize()
+    {
+        instance = FindObjectOfType(typeof(HoloRecorder)) as HoloRecorder;
+        if (!MixedRealityServiceRegistry.TryGetService<IMixedRealityInputSystem>(out inputSystem))
+        {
+            Debug.Log("Failed to acquire the input system. It may not have been registered");
+        }
+        animationRecorder = new InputRecordingService(inputSystem);
+    }
+
+    public void StartRecording()
+    {
+        currentAudioClip = Microphone.Start(MICROPHONE_NAME, true, 10, 44100);
+        animationRecorder.StartRecording();
+    }
+
+    public HoloRecording StopRecording()
+    {
+        Microphone.End(MICROPHONE_NAME);
+        animationRecorder.StopRecording();
+        string animationClipFilePath = animationRecorder.SaveInputAnimation("recorded-animation");
+        Debug.Log($"The file path of the animation is: {animationClipFilePath}");
+        return new HoloRecording(currentAudioClip, animationClipFilePath);
+    }
+
+    public void CancelRecording()
+    {
+        animationRecorder.DiscardRecordedInput();
+        Microphone.End(MICROPHONE_NAME);
+        currentAudioClip = null;
+    }
+}
---
+++ Assets/Scripts/HoloRecording.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloRecording
+{
+    public AudioClip audioClip;
+    public string pathToAnimationFile;
+    public HoloRecording(AudioClip audioClip, string pathToAnimationFile)
+    {
+        this.audioClip = audioClip;
+        this.pathToAnimationFile = pathToAnimationFile;
+    }
+}
