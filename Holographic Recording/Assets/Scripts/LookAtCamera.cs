5a48071a0fe509384f42b766f3660ceb2aff3f9f
Isaak <isaakh@ethz.ch>
2020-10-29 17:35:32 +0100

    Make Recording Representation look at camera

---
+++ Holographic Recording/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera: MonoBehaviour
+{
+
+    private Camera mainCamera;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        gameObject.transform.LookAt(mainCamera.transform);
+    }
+}
