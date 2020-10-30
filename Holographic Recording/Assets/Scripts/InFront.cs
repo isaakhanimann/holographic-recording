503145f0ea8a68df161270bbb506727e2af7bd0c
Isaak <isaakh@ethz.ch>
2020-10-30 12:01:07 +0100

    Add Timer in front of camera, not updating time yet

---
+++ Holographic Recording/Assets/Scripts/InFront.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+public class InFront : Solver
+{
+
+    public override void SolverUpdate()
+    {
+        if (SolverHandler != null && SolverHandler.TransformTarget != null)
+        {
+            var target = SolverHandler.TransformTarget;
+            GoalPosition = target.position + target.forward * 0.8f;
+            GoalRotation = target.rotation;
+        }
+    }
+}
