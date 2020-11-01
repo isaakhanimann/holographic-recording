bcdd437286bf6b775c5829dc74c15bd57a191ac9
Isaak <isaakh@ethz.ch>
2020-11-01 12:19:11 +0100

    Make functions in HoloPlayerBehaviour more modular

--- Holographic Recording/Assets/Scripts/HoloPlayerBehaviour.cs
+++ Holographic Recording/Assets/Scripts/HoloPlayerBehaviour.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using System.IO;
-
+using JetBrains.Annotations;
 
 public class HoloPlayerBehaviour : MonoBehaviour
 { 
@@ -37,7 +37,6 @@ public class HoloPlayerBehaviour : MonoBehaviour
         instantiatedHands.SetActive(false);
     }
 
-
     public void Play()
     {
         debugLogsObject.GetComponent<TextMeshPro>().text += "Play" + System.Environment.NewLine;
@@ -53,19 +52,50 @@ public class HoloPlayerBehaviour : MonoBehaviour
         instantiatedHands.SetActive(false);
     }
 
-
     private AnimationClip GetAnimationClipFromPath(string path)
     {
         string keyframesAsJson = File.ReadAllText(path);
         SerializableSnapshots serializableSnapshots = JsonUtility.FromJson<SerializableSnapshots>(keyframesAsJson);
-        return GetAnimationClipFromRecordedKeyframes(serializableSnapshots);
+        return CreateAnimationClipFromRecordedSnapshots(serializableSnapshots);
     }
 
-
-    private AnimationClip GetAnimationClipFromRecordedKeyframes(SerializableSnapshots serializableSnapshots)
+    private AnimationClip CreateAnimationClipFromRecordedSnapshots(SerializableSnapshots serializableSnapshots)
     {
         int numberOfKeyframes = serializableSnapshots.snapshots.Count;
         lengthOfAnimationInSeconds = serializableSnapshots.snapshots[numberOfKeyframes - 1].time;
+
+        CurvesForAllJoints curvesForAllJoints = CreateCurvesForAllJoints(serializableSnapshots.snapshots);
+
+        return CreateAnimationClipFromCurves(curvesForAllJoints);
+    }
+
+    private AnimationClip CreateAnimationClipFromCurves(CurvesForAllJoints curvesForAllJoints)
+    {
+        AnimationClip newClip = new AnimationClip();
+        newClip.legacy = true;
+        string pathToLeftPalm = "HandRig_L";
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.x", curvesForAllJoints.leftPalmCurves.translateX);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.y", curvesForAllJoints.leftPalmCurves.translateY);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.z", curvesForAllJoints.leftPalmCurves.translateZ);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.x", curvesForAllJoints.leftPalmCurves.rotateX);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.y", curvesForAllJoints.leftPalmCurves.rotateY);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.z", curvesForAllJoints.leftPalmCurves.rotateZ);
+        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.w", curvesForAllJoints.leftPalmCurves.rotateW);
+
+        string pathToRightPalm = "HandRig_R";
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.x", curvesForAllJoints.rightPalmCurves.translateX);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.y", curvesForAllJoints.rightPalmCurves.translateY);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.z", curvesForAllJoints.rightPalmCurves.translateZ);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.x", curvesForAllJoints.rightPalmCurves.rotateX);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.y", curvesForAllJoints.rightPalmCurves.rotateY);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.z", curvesForAllJoints.rightPalmCurves.rotateZ);
+        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.w", curvesForAllJoints.rightPalmCurves.rotateW);
+        return newClip;
+    }
+
+    private CurvesForAllJoints CreateCurvesForAllJoints(List<Snapshot> snapshots)
+    {
+        int numberOfKeyframes = snapshots.Count;
 
         Keyframe[] leftKeyframesX = new Keyframe[numberOfKeyframes];
         Keyframe[] leftKeyframesY = new Keyframe[numberOfKeyframes];
@@ -82,9 +112,10 @@ public class HoloPlayerBehaviour : MonoBehaviour
         Keyframe[] rightKeyframesRotationY = new Keyframe[numberOfKeyframes];
         Keyframe[] rightKeyframesRotationZ = new Keyframe[numberOfKeyframes];
         Keyframe[] rightKeyframesRotationW = new Keyframe[numberOfKeyframes];
+
         for (int i = 0; i < numberOfKeyframes; i++)
         {
-            Snapshot snapshot = serializableSnapshots.snapshots[i];
+            Snapshot snapshot = snapshots[i];
             float time = snapshot.time;
 
             leftKeyframesX[i] = new Keyframe(time, snapshot.leftPalm.positionX);
@@ -103,47 +134,53 @@ public class HoloPlayerBehaviour : MonoBehaviour
             rightKeyframesRotationZ[i] = new Keyframe(time, snapshot.rightPalm.rotationZ);
             rightKeyframesRotationW[i] = new Keyframe(time, snapshot.rightPalm.rotationW);
         }
-        
 
-        AnimationCurve leftTranslateX = new AnimationCurve(leftKeyframesX);
-        AnimationCurve leftTranslateY = new AnimationCurve(leftKeyframesY);
-        AnimationCurve leftTranslateZ = new AnimationCurve(leftKeyframesZ);
-        AnimationCurve leftRotateX = new AnimationCurve(leftKeyframesRotationX);
-        AnimationCurve leftRotateY = new AnimationCurve(leftKeyframesRotationY);
-        AnimationCurve leftRotateZ = new AnimationCurve(leftKeyframesRotationZ);
-        AnimationCurve leftRotateW = new AnimationCurve(leftKeyframesRotationW);
+        JointCurves leftPalmCurves = new JointCurves();
+        leftPalmCurves.translateX = new AnimationCurve(leftKeyframesX);
+        leftPalmCurves.translateY = new AnimationCurve(leftKeyframesY);
+        leftPalmCurves.translateZ = new AnimationCurve(leftKeyframesZ);
+        leftPalmCurves.rotateX = new AnimationCurve(leftKeyframesRotationX);
+        leftPalmCurves.rotateY = new AnimationCurve(leftKeyframesRotationY);
+        leftPalmCurves.rotateZ = new AnimationCurve(leftKeyframesRotationZ);
+        leftPalmCurves.rotateW = new AnimationCurve(leftKeyframesRotationW);
 
-        AnimationCurve rightTranslateX = new AnimationCurve(rightKeyframesX);
-        AnimationCurve rightTranslateY = new AnimationCurve(rightKeyframesY);
-        AnimationCurve rightTranslateZ = new AnimationCurve(rightKeyframesZ);
-        AnimationCurve rightRotateX = new AnimationCurve(rightKeyframesRotationX);
-        AnimationCurve rightRotateY = new AnimationCurve(rightKeyframesRotationY);
-        AnimationCurve rightRotateZ = new AnimationCurve(rightKeyframesRotationZ);
-        AnimationCurve rightRotateW = new AnimationCurve(rightKeyframesRotationW);
+        JointCurves rightPalmCurves = new JointCurves();
+        rightPalmCurves.translateX = new AnimationCurve(rightKeyframesX);
+        rightPalmCurves.translateY = new AnimationCurve(rightKeyframesY);
+        rightPalmCurves.translateZ = new AnimationCurve(rightKeyframesZ);
+        rightPalmCurves.rotateX = new AnimationCurve(rightKeyframesRotationX);
+        rightPalmCurves.rotateY = new AnimationCurve(rightKeyframesRotationY);
+        rightPalmCurves.rotateZ = new AnimationCurve(rightKeyframesRotationZ);
+        rightPalmCurves.rotateW = new AnimationCurve(rightKeyframesRotationW);
 
+        CurvesForAllJoints allCurves = new CurvesForAllJoints();
 
-        AnimationClip newClip = new AnimationClip();
-        newClip.legacy = true;
-        string pathToLeftPalm = "HandRig_L";
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.x", leftTranslateX);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.y", leftTranslateY);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localPosition.z", leftTranslateZ);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.x", leftRotateX);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.y", leftRotateY);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.z", leftRotateZ);
-        newClip.SetCurve(pathToLeftPalm, typeof(Transform), "localRotation.w", leftRotateW);
+        allCurves.leftPalmCurves = leftPalmCurves;
+        allCurves.rightPalmCurves = rightPalmCurves;
+        return allCurves;
+    }
 
-        string pathToRightPalm = "HandRig_R";
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.x", rightTranslateX);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.y", rightTranslateY);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localPosition.z", rightTranslateZ);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.x", rightRotateX);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.y", rightRotateY);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.z", rightRotateZ);
-        newClip.SetCurve(pathToRightPalm, typeof(Transform), "localRotation.w", rightRotateW);
-        return newClip;
+    private class CurvesForAllJoints
+    {
+        public JointCurves leftPalmCurves;
+        public JointCurves rightPalmCurves;
+    }
+
+    private class JointCurves
+    {
+        public AnimationCurve translateX;
+        public AnimationCurve translateY;
+        public AnimationCurve translateZ;
+
+        public AnimationCurve rotateX;
+        public AnimationCurve rotateY;
+        public AnimationCurve rotateZ;
+        public AnimationCurve rotateW;
     }
 
 
 
 }
+
+
+
