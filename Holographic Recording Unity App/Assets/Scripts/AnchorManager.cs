6c71570781d896e6496d785532f41a78994dd443
xiaxia <xiaxia@ethz.ch>
2020-12-02 17:36:43 +0100

    add debug panel and log to it with callbacks

--- Holographic Recording Unity App/Assets/Scripts/AnchorManager.cs
+++ Holographic Recording Unity App/Assets/Scripts/AnchorManager.cs
@@ -6,58 +6,83 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using TMPro;
 
 public class AnchorManager : DemoScriptBase
 {
-  public override Task AdvanceDemoAsync()
-  {
-	throw new NotImplementedException();
-  }
 
-  protected override Color GetStepColor()
-  {
-	throw new NotImplementedException();
-  }
+    public TextMeshPro debugText;
+    public override Task AdvanceDemoAsync()
+    {
+    throw new NotImplementedException();
+    }
 
-  protected override bool IsPlacingObject()
-  {
-	throw new NotImplementedException();
-  }
+    protected override Color GetStepColor()
+    {
+    throw new NotImplementedException();
+    }
 
-  void Start()
-  {
-	SpawnNewAnchoredObject(new Vector3(0.1f, 0, 0), new Quaternion(0, 0, 0, 1));
-	SaveAnchor();
-  }
+    protected override bool IsPlacingObject()
+    {
+    throw new NotImplementedException();
+    }
 
-  private async Task SaveAnchor()
-  {
-	await SaveCurrentObjectAnchorToCloudAsync();
-  }
+    void Start()
+    {
+    SpawnNewAnchoredObject(new Vector3(0.1f, 0, 0), new Quaternion(0, 0, 0, 1));
+    SaveAnchor();
+    }
 
-  protected override void OnCloudAnchorLocated(AnchorLocatedEventArgs args)
-  {
+    private async Task SaveAnchor()
+    {
+    await SaveCurrentObjectAnchorToCloudAsync();
+    }
+
+    protected override void OnCloudAnchorLocated(AnchorLocatedEventArgs args)
+    {
     base.OnCloudAnchorLocated(args);
 
-    if (args.Status == LocateAnchorStatus.Located)
-    {
-      currentCloudAnchor = args.Anchor;
+        debugText.text += "Anchor loaded \n";
 
-      UnityDispatcher.InvokeOnAppThread(() =>
-      {
+
+        if (args.Status == LocateAnchorStatus.Located)
+    {
+        currentCloudAnchor = args.Anchor;
+
+        UnityDispatcher.InvokeOnAppThread(() =>
+        {
         // currentAppState = AppState.DemoStepDeleteFoundAnchor;
         Pose anchorPose = Pose.identity;
 
         // HoloLens: The position will be set based on the unityARUserAnchor that was located.
         SpawnOrMoveCurrentAnchoredObject(anchorPose.position, anchorPose.rotation);
-      });
+        });
     }
-  }
+    }
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Update is called once per frame
+    void Update()
+    {
 
-  }
+    }
+
+    protected override async Task OnSaveCloudAnchorSuccessfulAsync()
+
+    {
+
+        await base.OnSaveCloudAnchorSuccessfulAsync();
+
+        debugText.text += "Save cloud anchor successful \n";
+
+    }
+
+    protected override void OnSaveCloudAnchorFailed(Exception exception)
+
+    {
+
+        base.OnSaveCloudAnchorFailed(exception);
+        
+        debugText.text += "Save cloud anchor failed \n";
+    }
 
 }
