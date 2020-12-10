c2f91ae3265a0697fefba36b745df9fe3a791a53
Isaak <isaakh@ethz.ch>
2020-12-10 13:25:32 +0100

    Disable Developer Console

---
+++ Holographic Recording/Assets/Scripts/DisableDeveloperConsole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableDeveloperConsole : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Debug.developerConsoleVisible == true)
+        {
+            Debug.developerConsoleVisible = false;
+        }
+    }
+}
